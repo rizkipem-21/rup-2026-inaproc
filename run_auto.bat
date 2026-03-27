5f19735af974feb4da58123f4d0b98e371e45b39
rizkipem-21 <rizki.pem@gmail.com>
2026-03-27 15:47:32 +0700

    auto update 27/03/2026 15.47.32,25 WIB

--- run_auto.bat
+++ run_auto.bat
@@ -1,26 +1,25 @@
 @echo off
 cd /d D:\rup-2026-inaproc
 
-echo =========================
-echo DOWNLOAD DATA INAPROC
-echo =========================
-powershell -ExecutionPolicy Bypass -File download.ps1
+echo ========================= >> log.txt
+echo START %date% %time% >> log.txt
 
-echo.
-echo =========================
-echo GENERATE REKAP
-echo =========================
-python generate_rekap.py
+echo DOWNLOAD DATA >> log.txt
+powershell -ExecutionPolicy Bypass -File download.ps1 >> log.txt 2>&1
 
-echo.
-echo =========================
-echo PUSH KE GITHUB
-echo =========================
+echo. >> log.txt
+echo GENERATE REKAP >> log.txt
+python generate_rekap.py >> log.txt 2>&1
 
-git add .
-git commit -m "auto update %date% %time%" || echo tidak ada perubahan
-git push origin main
+echo. >> log.txt
+echo PUSH KE GITHUB >> log.txt
 
-echo.
-echo SELESAI
-exit
+git add . >> log.txt 2>&1
+
+git commit -m "auto update %date% %time% WIB" --allow-empty >> log.txt 2>&1
+
+git push origin main >> log.txt 2>&1
+
+echo. >> log.txt
+echo DONE %date% %time% >> log.txt
+echo ========================= >> log.txt
