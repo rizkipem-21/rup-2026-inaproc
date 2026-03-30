43139f973759aaf3639d104fc23f6687ce316b1d
rizkipem-21 <rizki.pem@gmail.com>
2026-03-30 14:35:04 +0700

    auto update 30/03/2026 14.35.03,88

--- run_auto.bat
+++ run_auto.bat
@@ -1,47 +1,28 @@
 @echo off
+setlocal
 
 cd /d D:\rup-2026-inaproc
 
-timeout /t 120 /nobreak
+echo ========================= >> log.txt
+echo START %date% %time% >> log.txt
 
-set LOGFILE=log.txt
+echo DOWNLOAD DATA >> log.txt
+powershell -ExecutionPolicy Bypass -File download.ps1 >> log.txt 2>&1
 
-echo ========================= >> %LOGFILE%
-echo START %date% %time% >> %LOGFILE%
+echo GENERATE REKAP >> log.txt
+"C:\Users\Rizki FN LPSE Kab. Kotawaringin Barat\AppData\Local\Programs\Python\Python311\python.exe" generate_rekap.py >> log.txt 2>&1
 
-REM =========================
-REM FIX GIT SAFE DIRECTORY
-REM =========================
-"C:\Program Files\Git\bin\git.exe" config --global --add safe.directory D:/rup-2026-inaproc >> %LOGFILE% 2>&1
+echo GIT CONFIG >> log.txt
+git config --global --add safe.directory D:/rup-2026-inaproc >> log.txt 2>&1
 
-REM =========================
-REM DOWNLOAD DATA
-REM =========================
-echo DOWNLOAD DATA >> %LOGFILE%
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File download.ps1 >> %LOGFILE% 2>&1
+echo GIT ADD >> log.txt
+git add . >> log.txt 2>&1
 
-REM =========================
-REM GENERATE REKAP (FULL PATH PYTHON)
-REM =========================
-echo GENERATE REKAP >> %LOGFILE%
-"C:\Users\Rizki\AppData\Local\Programs\Python\Python311\python.exe" generate_rekap.py >> %LOGFILE% 2>&1
+echo GIT COMMIT >> log.txt
+git commit -m "auto update %date% %time%" >> log.txt 2>&1
 
-REM =========================
-REM GIT PROCESS
-REM =========================
-echo GIT ADD >> %LOGFILE%
-"C:\Program Files\Git\bin\git.exe" add . >> %LOGFILE% 2>&1
+echo GIT PUSH >> log.txt
+git push >> log.txt 2>&1
 
-echo GIT COMMIT >> %LOGFILE%
-"C:\Program Files\Git\bin\git.exe" commit -m "auto update %date% %time%" >> %LOGFILE% 2>&1
-
-echo GIT PULL >> %LOGFILE%
-"C:\Program Files\Git\bin\git.exe" pull origin main --rebase >> %LOGFILE% 2>&1
-
-echo GIT PUSH >> %LOGFILE%
-"C:\Program Files\Git\bin\git.exe" push origin main >> %LOGFILE% 2>&1
-
-echo SELESAI %date% %time% >> %LOGFILE%
-echo ========================= >> %LOGFILE%
-
-exit
+echo SELESAI %date% %time% >> log.txt
+echo ========================= >> log.txt
